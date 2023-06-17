e34c6ae04a4dd181840493321c30d7820c6cb792
Deivis <davidesparza9090@gmail.com>
2023-06-17 11:18:09 -0600

    Implementación de Accesibilidad en Proyecto...
    Se ingreso el codigo base para las opciones de accesbilidad en las paginas de Home, Galeria, y About...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,4 +6,113 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  Hablar(){
+
+    const X = document.getElementById('Titulo')?.innerHTML;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(X));
+
+    const X1 = document.getElementById('Contenido')?.innerHTML;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(X1));
+
+    const X2 = document.getElementById('Contenido2')?.innerHTML;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(X2));
+
+  }
+
+  Pausar(){
+
+    speechSynthesis.pause();
+
+  }
+
+  Resumen(){
+
+    speechSynthesis.resume();
+
+  }
+
+  // CONTRASTE DE ENLACES -----------------------------------------------------------------------------------------------
+
+  EnlacesB(){
+
+    const X = document.getElementById('Enlace') as HTMLAnchorElement;
+    const X2 = document.getElementById('Enlace2') as HTMLAnchorElement;
+    const X3 = document.getElementById('Enlace3') as HTMLAnchorElement;
+
+    X.style.backgroundColor = 'black';
+    X.style.color = 'yellow';
+
+    X2.style.backgroundColor = 'black';
+    X2.style.color = 'yellow';
+
+    X3.style.backgroundColor = 'black';
+    X3.style.color = 'yellow';
+  }
+
+  EnlacesA(){
+
+    const X = document.getElementById('Enlace') as HTMLAnchorElement;
+    const X2 = document.getElementById('Enlace2') as HTMLAnchorElement;
+    const X3 = document.getElementById('Enlace3') as HTMLAnchorElement;
+
+    X.style.backgroundColor = 'white';
+    X.style.color = 'blue';
+
+    X2.style.backgroundColor = 'white';
+    X2.style.color = 'blue';
+
+    X3.style.backgroundColor = 'white';
+    X3.style.color = 'blue';
+
+  }
+
+  // CONTRASTE DE PAGINA -----------------------------------------------------------------------------------------------
+
+  ContrasteA(){
+
+    const X = document.getElementById('Cuerpo') as HTMLBodyElement;
+
+    X.style.backgroundColor = 'black';
+    X.style.color = 'yellow';
+  }
+
+  ContrasteB(){
+
+    const X = document.getElementById('Cuerpo') as HTMLBodyElement;
+
+    X.style.backgroundColor = 'white';
+    X.style.color = 'black';
+  }
+
+  // CAMBIO DE ZOOM ----------------------------------------------------------------------------------------------------
+
+  ZoomMas(){
+
+    var I = 3;
+    var TAM = new Array ('xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large');
+    const X = document.getElementById('Cuerpo') as HTMLDivElement;
+
+    X.style.fontSize = TAM[I+2];
+    
+  }
+
+  ZoomMenos(){
+
+    var I = 3;
+    var TAM = new Array ('xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large');
+    const X = document.getElementById('Cuerpo') as HTMLDivElement;
+  
+    X.style.fontSize = TAM[I-2];
+    
+  }
+
+  ZoomNormal(){
+
+    var I = 3;
+    var TAM = new Array ('xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large');
+    const X = document.getElementById('Cuerpo') as HTMLDivElement;
+  
+    X.style.fontSize = TAM[I];
+    
+  }
 }
