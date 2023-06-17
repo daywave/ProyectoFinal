b3198a53caa1234aa48c36fd330f19495436200f
Carlos Daniel de Lira Salas <99099769+CDanielLira@users.noreply.github.com>
2023-06-16 23:50:17 -0600

    Hola mundo

--- src/app/habitacion.service.ts
+++ src/app/habitacion.service.ts
@@ -7,7 +7,7 @@ import { Observable, of } from 'rxjs';
 export class HabitacionService {
 
   private habitaciones: any[] = [
-    { id: 1, nombre: 'Habitación individual', tipo: 'Individual', precio: 500, descripcion: 'Habitación para una persona', imagen: 'https://bufondearenillashotel.es/wp-content/uploads/2015/02/habitacion-individual.jpg' },
+    { id: 1, nombre: 'El cochi es puto', tipo: 'Individual', precio: 500, descripcion: 'Habitación para una persona', imagen: 'https://bufondearenillashotel.es/wp-content/uploads/2015/02/habitacion-individual.jpg' },
     { id: 2, nombre: 'Habitación doble', tipo: 'Doble', precio: 800, descripcion: 'Habitación para dos personas', imagen: 'https://hotelmalinallihuamantla.com/wp-content/uploads/2019/12/habitacion-doble-malinalli-1.jpg' },
     { id: 3, nombre: 'Habitación triple', tipo: 'Triple', precio: 1100, descripcion: 'Habitación para tres personas', imagen: 'https://hotelflamingoinn.com.mx/wp-content/uploads/2021/05/habitacion-ejecuiva-triple.jpg' },
     { id: 4, nombre: 'Suite', tipo: 'Suite', precio: 1500, descripcion: 'Habitación de lujo para dos personas', imagen: 'https://s3.us-east-1.amazonaws.com/comercial-production-gx-cms-content-bucket/hxm/rooms/suites/ocean-front-family/ocean-front-fam-005.jpg' },
