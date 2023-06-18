2c33072fa000c13e85e294c67d11113132f68338
Daywave <djjceja@gmail.com>
2023-06-17 20:57:46 -0600

    correo funcional

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,29 +1,45 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
   forma: FormGroup;
-  usuario:any={
-    nombre:"",
-    apellido:"",
-    correo:"" }
+  usuario: any = {
+    nombre: "",
+    apellido: "",
+    correo: ""
+  };
 
-  constructor(){
+  constructor(private http: HttpClient) {
     this.forma = new FormGroup({
       'nombre': new FormControl(this.usuario.nombre, [Validators.required, Validators.minLength(3)]),
       'apellido': new FormControl(this.usuario.apellido, Validators.required),
-      'correo': new FormControl(this.usuario.correo, [Validators.required,Validators.email])
+      'correo': new FormControl(this.usuario.correo, [Validators.required, Validators.email])
     });
   }
 
-  guardarCambios(): void{
+  guardarCambios(): void {
     console.log("metodo guardarCambios");
     console.log(this.forma);
     console.log(this.forma.value);
+
+    const formData = {
+      name: "FormSubmit",
+      message: "I'm from Devro LABS"
+    };
+
+    this.http.post('http://localhost:3000/send-email', formData)
+      .subscribe(response => {
+        console.log(response);
+        // Puedes realizar acciones adicionales después de enviar el correo
+      }, error => {
+        console.error(error);
+        // Puedes manejar los errores aquí
+      });
   }
 }
